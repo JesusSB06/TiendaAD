9147f268e1d608ec2cb46e456ca098a6619aa193
Sharkoise <a24roicf@iesantonlosada.gal>
2026-01-12 14:39:42 +0100

    Base proyecto tienda

---
+++ src/main/main.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ *
+ * @author dam2_alu03@inf.ald
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //TODO
+    }
+    
+}

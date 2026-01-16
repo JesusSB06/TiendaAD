269155306134e3b911a3632eb6c9104402b73ae6
JesusSB06 <a24jesussb@iesantonlosada.gal>
2026-01-16 10:50:21 +0100

    Parte de la vista central de productos hecha

--- src/main/main.java
+++ src/main/main.java
@@ -1,8 +1,6 @@
 package main;
 
 import controller.MainController;
-import java.sql.SQLException;
-import model.OperationsDB;
 import view.MainJFrame;
 
 /**
@@ -11,20 +9,14 @@ import view.MainJFrame;
  */
 public class main {
 
-    private static OperationsDB conexion = new OperationsDB();
-
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws SQLException {
-        conexion.openConnection();
-        conexion.consultaProducto();
-        conexion.closeConnection();
-
-        /* MainJFrame mainJFrame = new MainJFrame();
+    public static void main(String[] args) {
+        MainJFrame mainJFrame = new MainJFrame();
         mainJFrame.setVisible(true);
         MainController mc = new MainController(mainJFrame);
-        mc.changeImage();*/
+        mc.changeImage();
     }
-
+    
 }
--- src/model/OperationsDB.java
+++ src/model/OperationsDB.java
@@ -2,10 +2,7 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import main.main;
 
 /**
@@ -14,52 +11,19 @@ import main.main;
  */
 public class OperationsDB {
 
-    private static Connection conexion;
+    private static Connection conexion = null;
 
     public static void openConnection() {
         try {
-            System.out.println("Abriendo conexion");
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String url = "jdbc:mysql://localhost:3306/tienda_inf";
+            String url = "jdbc:mysql://localhost:3306/BDTIENDAINFORMATICA";
             conexion = DriverManager.getConnection(url, "root", "abc123.");
         } catch (ClassNotFoundException | SQLException ex) {
             System.getLogger(main.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
         }
     }
 
-    public void closeConnection() throws SQLException {
+    public static void closeConnection() throws SQLException {
         conexion.close();
-    }
-
-    public static void consultaProducto() throws SQLException {
-        System.out.println("Consulta producto");
-        String select = "SELECT * from producto";
-        Statement st = conexion.createStatement();
-        ResultSet rs = st.executeQuery(select);
-        while (rs.next()) {
-            System.out.println("-- Producto --");
-            System.out.println("ID: " + rs.getString("id_producto"));
-            System.out.println("Nombre: " + rs.getString("nombre_producto"));
-            System.out.println("Stock: " + rs.getInt("stock"));
-            System.out.println("Estado: " + rs.getString("estado"));
-            System.out.println("Precio: " + rs.getDouble("precio"));
-            System.out.println("Categoria: " + rs.getString("categoria"));
-
-        }
-    }
-
-    public int addProduct(Product product) throws SQLException {
-        int vId = product.getId();
-        String vName = product.getName();
-        int vStock = product.getStock();
-        String vState = product.getState();
-        Double vPrice = product.getPrice();
-        String vImg = product.getImg();
-        Category vCategory = product.getCategory();
-        String sentenciaSQL = "INSERT into Product (id_producto, nombre_producto, stock, estado, price, img, category) values (?,?,?,?,?,?,?)";
-        PreparedStatement ps = conexion.prepareStatement(sentenciaSQL);
-        int resultado = ps.executeUpdate();
-        ps.close();
-        return resultado;
     }
 }

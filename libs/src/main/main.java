902c12c751984551184811f0b7cbe6f5451f4e2b
JesusSB06 <a24jesussb@iesantonlosada.gal>
2026-01-19 14:47:37 +0100

    Commit: creación de la vista a la que accederán los clientes

---
+++ libs/src/main/main.java
@@ -0,0 +1,28 @@
+package main;
+
+import controller.MainController;
+import java.sql.SQLException;
+import model.OperationsDB;
+import view.MainJFrame;
+
+/**
+ *
+ * @author dam2_alu03@inf.ald
+ */
+public class main {
+    private static OperationsDB conexion = new OperationsDB();
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+
+        
+        MainJFrame mainJFrame = new MainJFrame();
+        mainJFrame.setVisible(true);
+        MainController mc = new MainController(mainJFrame);
+        mc.changeImage();
+        
+        
+    }
+    
+}
---
+++ libs/src/model/Technician.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.time.LocalTime;
+
+/**
+ *
+ * @author dam2_alu03@inf.ald
+ */
+public class Technician extends Employee {
+    
+    public Technician(int id, String dni, String name, String email, String direction, Employee supervisor, LocalTime startHour, LocalTime exitTime) {
+        super(id, dni, name, email, direction, supervisor, startHour, exitTime);
+    }
+    
+
+}
